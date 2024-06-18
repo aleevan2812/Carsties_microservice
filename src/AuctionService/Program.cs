011f2353a58db5c39beee288d7312ea265ca6a0c
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-06-18 16:03:57 +0700

    02 - 008 Shaping the data to return

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. 
