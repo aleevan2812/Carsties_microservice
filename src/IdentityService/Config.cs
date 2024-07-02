dbf2b3842db8dddaab4be21606f944a21c9ab993
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-02 23:40:30 +0700

    05 - 010 Adding client credentials to allow clients to request a token

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -7,46 +7,27 @@ public static class Config
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
         {
-            new IdentityResources.OpenId(),
+            new IdentityResources.OpenId(), // using OpenID Connect means that we get 2 token back: access token and ID token (contains information of user)
             new IdentityResources.Profile()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new("scope1"),
-            new("scope2")
+            new("auctionApp", "Auction app full access")
         };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // m2m client credentials flow client
             new()
             {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
-
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                AllowedScopes = { "scope1" }
-            },
-
-            // interactive client using code flow + pkce
-            new()
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
+                ClientId = "postman",
+                ClientName = "Postman",
+                AllowedScopes = { "openid", "profile", "auctionApp" },
+                RedirectUris = { "https://www.getpostman.com/oauth2/callback" },
+                ClientSecrets = new[] { new Secret("NotASecret".Sha256()) },
+                AllowedGrantTypes = { GrantType.ResourceOwnerPassword }
             }
         };
 }
