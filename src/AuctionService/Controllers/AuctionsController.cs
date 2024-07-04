400f6d31e34bbb199d1e85eb5c10f7bc839ebe64
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-04 22:16:40 +0700

    05 - 013 Configuring the auth endpoints on the resource server

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Contracts;
 using MassTransit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -48,12 +49,13 @@ public class AuctionsController : ControllerBase
         return _mapper.Map<AuctionDto>(auction);
     }
 
+    [Authorize]
     [HttpPost]
     public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
     {
         var auction = _mapper.Map<Auction>(auctionDto);
         // TODO: add current user as seller
-        auction.Seller = "test";
+        auction.Seller = User.Identity.Name;
 
         _context.Auctions.Add(auction);
 
@@ -69,6 +71,7 @@ public class AuctionsController : ControllerBase
             new { auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
 
+    [Authorize]
     [HttpPut("{id}")]
     public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
     {
@@ -78,6 +81,7 @@ public class AuctionsController : ControllerBase
         if (auction == null) return NotFound();
 
         // TODO: check seller == username
+        if (auction.Seller != User.Identity.Name) return Forbid();
 
         auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
         auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
@@ -94,6 +98,7 @@ public class AuctionsController : ControllerBase
         return BadRequest("Problem saving changes");
     }
 
+    [Authorize]
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteAuction(Guid id)
     {
@@ -102,6 +107,7 @@ public class AuctionsController : ControllerBase
         if (auction == null) return NotFound();
 
         // TODO: check seller == username
+        if (auction.Seller != User.Identity.Name) return Forbid();
 
         _context.Auctions.Remove(auction);
 
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -33,13 +33,23 @@ builder.Services.AddMassTransit(x =>
     x.UsingRabbitMq((context, cfg) => { cfg.ConfigureEndpoints(context); });
 });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.Authority = builder.Configuration["IdentityServiceUrl"];
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters.ValidateAudience = false;
-    options.TokenValidationParameters.NameClaimType = "username";
-});
+builder.Services
+    // Thêm dịch vụ xác thực vào ứng dụng và chỉ định rằng sẽ sử dụng xác thực JWT Bearer.
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // Đặt địa chỉ authority (URL của nhà cung cấp định danh)
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+
+        // Tắt yêu cầu phải sử dụng HTTPS cho metadata. Điều này có thể hữu ích trong quá trình phát triển và kiểm thử, nhưng không nên sử dụng trong môi trường sản xuất.
+        options.RequireHttpsMetadata = false;
+
+        // Không xác thực audience của token. Điều này có nghĩa là ứng dụng sẽ không kiểm tra xem token có được phát hành cho đối tượng cụ thể nào hay không.
+        options.TokenValidationParameters.ValidateAudience = false;
+
+        // Đặt loại claim dùng làm tên người dùng là "username". Điều này có nghĩa là khi token được xác thực, giá trị của claim "username" sẽ được sử dụng làm tên người dùng trong ứng dụng.
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
 
 var app = builder.Build();
 
