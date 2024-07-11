15077a8ac1a8941f60000e04b0dad60e1b038594
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-11 22:24:15 +0700

    08 - 007 Styling the auction cards

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {
@@ -5,5 +6,26 @@ type Props = {
 };
 
 export default function AuctionCard({ auction }: Props) {
-  return <div>{auction.make}</div>;
+  return (
+    <a href="#">
+      <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
+        <div>
+          <Image
+            src={auction.imageUrl}
+            alt="image"
+            fill
+            priority
+            className="object-cover"
+            sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+          />
+        </div>
+      </div>
+      <div className="flex justify-between items-center mt-4">
+        <h3 className="text-gray-700">
+          {auction.make} {auction.model}
+        </h3>
+        <p className="font-semibold text-sm">{auction.year}</p>
+      </div>
+    </a>
+  );
 }
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -5,7 +5,7 @@ import { json } from "stream/consumers";
 import AuctionCard from "./AuctionCard";
 
 async function getData() {
-  const res = await fetch("http://localhost:6001/search");
+  const res = await fetch("http://localhost:6001/search?pageSize=10");
 
   // console.log(res);
   if (!res.ok) throw new Error("Failed to fetch data");
@@ -16,7 +16,7 @@ async function getData() {
 export default async function Listings() {
   const data = await getData();
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-6">
       {data &&
         data.results.map((auction: any) => (
           <AuctionCard auction={auction} key={auction.id} />
