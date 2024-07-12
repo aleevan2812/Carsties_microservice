3087b59133515c038c22ea4ef9b7d7be7aabed30
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-12 15:31:15 +0700

    08 - 018 Adding a reset to the search function

--- frontend/web-app/app/Nav/Search.tsx
+++ frontend/web-app/app/Nav/Search.tsx
@@ -6,14 +6,15 @@ import { FaSearch } from "react-icons/fa";
 
 export default function Search() {
   const setParams = useParamsStore((state) => state.setParams);
-  const [value, setValue] = useState("");
+  const setSearchValue = useParamsStore((state) => state.setSearchValue);
+  const searchValue = useParamsStore((state) => state.searchValue);
 
   function onChange(event: any) {
-    setValue(event.target.value);
+    setSearchValue(event.target.value);
   }
 
   function search() {
-    setParams({ searchTerm: value });
+    setParams({ searchTerm: searchValue });
   }
 
   return (
@@ -22,6 +23,7 @@ export default function Search() {
         onKeyDown={(e: any) => {
           if (e.key === "Enter") search();
         }}
+        value={searchValue}
         onChange={onChange}
         type="text"
         placeholder="Search for cars by make, model or color"
