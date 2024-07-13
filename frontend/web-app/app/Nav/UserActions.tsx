b0264394b4460835979ce3e9b9223cabb5323ed6
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-14 00:00:46 +0700

    09 - 005 Getting the session details in the client

---
+++ frontend/web-app/app/Nav/UserActions.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "flowbite-react";
+import Link from "next/link";
+import React from "react";
+
+export default function UserActions() {
+  return (
+    <Button outline>
+      <Link href="/session">Session</Link>
+    </Button>
+  );
+}
