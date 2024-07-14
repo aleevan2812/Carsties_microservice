a3ef74f5ee831870194787440b66ee112d61ed4c
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-14 14:34:13 +0700

    09 - 006 Populating the session data

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d0ea6ac9feb8a7c2b250625a4ac30570a6c073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0264394b4460835979ce3e9b9223cabb5323ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
