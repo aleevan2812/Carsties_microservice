fa7d46341701445136c843926501ba0a1f9f9522
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-14 15:16:06 +0700

    09 - 010 Getting the access token to use to authenticate to our resource server

--- frontend/web-app/app/actions/authActions.ts
+++ frontend/web-app/app/actions/authActions.ts
@@ -1,5 +1,8 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { cookies, headers } from "next/headers";
+import { NextApiRequest } from "next";
+import { getToken } from "next-auth/jwt";
 
 export async function getSession() {
   return await getServerSession(authOptions);
@@ -15,4 +18,17 @@ export async function getCurrentUser() {
   } catch (error) {
     return null;
   }
+}
+
+export async function getTokenWorkaround() {
+  const req = {
+    headers: Object.fromEntries(headers() as Headers),
+    cookies: Object.fromEntries(
+      cookies()
+        .getAll()
+        .map((c) => [c.name, c.value])
+    ),
+  } as NextApiRequest;
+
+  return await getToken({ req });
 }
--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -16,8 +16,9 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, profile }) {
+    async jwt({ token, profile, account }) {
       if (profile) token.username = profile.username;
+      if (account) token.access_token = account.access_token;
       return token;
     },
     async session({ session, token }) {
