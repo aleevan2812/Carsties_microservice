cd589b62e8aa6968a294ef04bb9977478299737c
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-15 15:17:29 +0700

    11 - 002 Creating the Bid Service

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,8 +1,48 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x =>
+{
+	x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+		{
+			host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+			host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+		});
+
+		cfg.ConfigureEndpoints(context);
+	});
+});
+
+builder.Services
+	// Thêm dịch vụ xác thực vào ứng dụng và chỉ định rằng sẽ sử dụng xác thực JWT Bearer.
+	.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		// Đặt địa chỉ authority (URL của nhà cung cấp định danh)
+		options.Authority = builder.Configuration["IdentityServiceUrl"];
+
+		// Tắt yêu cầu phải sử dụng HTTPS cho metadata. Điều này có thể hữu ích trong quá trình phát triển và kiểm thử, nhưng không nên sử dụng trong môi trường sản xuất.
+		options.RequireHttpsMetadata = false;
+
+		// Không xác thực audience của token. Điều này có nghĩa là ứng dụng sẽ không kiểm tra xem token có được phát hành cho đối tượng cụ thể nào hay không.
+		options.TokenValidationParameters.ValidateAudience = false;
+
+		// Đặt loại claim dùng làm tên người dùng là "username". Điều này có nghĩa là khi token được xác thực, giá trị của claim "username" sẽ được sử dụng làm tên người dùng trong ứng dụng.
+		options.TokenValidationParameters.NameClaimType = "username";
+	});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +61,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("BidDb", MongoClientSettings
+	.FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
 
 app.Run();
