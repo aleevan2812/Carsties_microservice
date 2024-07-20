e4547c215bdb71ec3292695f80684080938fb12a
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-07-20 21:55:39 +0700

    13 - 012 Adding a toast for an auction created

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -7,13 +7,17 @@ import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 import { ReactNode, useEffect, useState } from "react";
 
-import { Bid } from "@/public/types";
+import { Auction, Bid } from "@/public/types";
+import { User } from "next-auth";
+import toast from "react-hot-toast";
+import AuctionCreatedToast from "../components/AuctionCreatedToast";
 
 type Props = {
   children: ReactNode;
+  user: User | null;
 };
 
-export default function SignalRProvider({ children }: Props) {
+export default function SignalRProvider({ children, user }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>(null);
   const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
   const addBid = useBidStore((state) => state.addBid);
@@ -39,6 +43,14 @@ export default function SignalRProvider({ children }: Props) {
               setCurrentPrice(bid.auctionId, bid.amount);
             }
             addBid(bid); // create at this lession
+          });
+
+          connection.on("AuctionCreated", (auction: Auction) => {
+            if (user?.username !== auction.seller) {
+              return toast(<AuctionCreatedToast auction={auction} />, {
+                duration: 10000,
+              });
+            }
           });
         })
         .catch((error) => console.log(error));
